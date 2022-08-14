80a1e764fef7b5e460773cb49d8b035125e001a2
jones2330 <johnsonandjonesjohnson@gmail.com>
2022-08-14 21:49:08 +0530

    cleared main method in writeCheck

--- src/test/java/com/test/WriteCheck.java
+++ src/test/java/com/test/WriteCheck.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class WriteCheck {
 	public static void main(String[] args) {
-		Scanner st = new Scanner(System.in);
-		String nextLine = st.nextLine();
-		System.out.println(nextLine);
+
 }
 }
